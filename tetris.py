60b373c1db8819c961d42f109074204e23261c77
shahdmohamedh <140630895+shahdmohamedh@users.noreply.github.com>
2025-04-28 21:51:46 +0300

    Add files via upload

--- tetris.py
+++ tetris.py
@@ -46,3 +46,35 @@ color, next_color = get_color(), get_color()
 
 score, lines = 0, 0
 scores = {0: 0, 1: 100, 2: 300, 3: 700, 4: 1500}
+  def check_borders():
+    if figure[i].x < 0 or figure[i].x > W - 1:
+        return False
+    elif figure[i].y > H - 1 or field[figure[i].y][figure[i].x]:
+        return False
+    return True
+
+
+def get_record():
+    try:
+        with open('record') as f:
+            return f.readline()
+    except FileNotFoundError:
+        with open('record', 'w') as f:
+            f.write('0')
+
+
+def set_record(record, score):
+    rec = max(int(record), score)
+    with open('record', 'w') as f:
+        f.write(str(rec))
+
+
+while True:
+    record = get_record()
+    dx, rotate = 0, False
+    sc.blit(bg, (0, 0))
+    sc.blit(game_sc, (20, 20))
+    game_sc.blit(game_bg, (0, 0))
+    # delay for full lines
+    for i in range(lines):
+        pygame.time.wait(200)
